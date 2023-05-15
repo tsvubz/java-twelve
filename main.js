43a5d9d12618458e414193e33ff78b56197903b4
Terence Svubure <tsvubure@gmail.com>
2023-05-15 15:52:27 +0200

    second

---
+++ main.js
@@ -0,0 +1,14 @@
+let n;
+
+function checkNumRange(n) {
+  if (n >= 100 && n <= 200) {
+    console.log('The number is in the range');
+  }else {
+    console.log('Number is not in range');
+  }
+}
+
+checkNumRange(180);
+checkNumRange(75);
+checkNumRange(115);
+checkNumRange(245);
